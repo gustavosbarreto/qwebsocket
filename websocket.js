fc094a3521917bf1413bd6689a40e006f49b581c
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2011-08-19 20:14:39 -0300

    Retrieve message data

--- websocket.js
+++ websocket.js
@@ -1,3 +1,3 @@
 var socket = new WebSocket(Bridge.getUrl());
 socket.onopen = function() { Bridge.emitOpened(); }
-socket.onmessage = function(data) { Bridge.emitMessageReceived(data); }
+socket.onmessage = function(event) { Bridge.emitMessageReceived(event.data); }
