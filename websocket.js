be96a60aee2bc51685d16fb16484c2c0fe1e401c
mscoolnerd <mscoolnerd@gmail.com>
2012-01-07 15:27:46 -0800

    Added support for socket.onerror and socket.onclose

--- websocket.js
+++ websocket.js
@@ -1,3 +1,5 @@
 var socket = new WebSocket(Bridge.getUrl());
 socket.onopen = function() { Bridge.emitOpened(); }
 socket.onmessage = function(event) { Bridge.emitMessageReceived(event.data); }
+socket.onerror = function(event) {Bridge.emitError(event.data); }
+socket.onclose = function(event) {Bridge.emitClose(); }
